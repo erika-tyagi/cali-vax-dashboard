1674038c952d600b3e0dc9094ab5b01c6fac127d
Erika Tyagi <erika.tyagi@gmail.com>
2020-03-08 22:10:00 -0500

    some stuff works

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import "./stylesheets/main.css";
 import * as d3 from "d3";
 import {select} from 'd3-selection';
 import {csv, json} from 'd3-fetch';
-
 import leafletSearch from 'leaflet-search'; 
 import leafletEasybutton from 'leaflet-easybutton'; 
 
-// DATA
+
+// ---------- DATA + RENDER ---------- //
 domReady(() => {
   Promise.all([
     json('./data/california.geojson'),
@@ -21,20 +21,24 @@ domReady(() => {
   });
 });
 
-// STRIP PLOT 
+// ---------- STRIP PLOT ---------- //
 function myStrip(caSchools) {
-  var margin = {top: 20, right: 70, bottom: 300, left: 10}; 
-  var width = 300 - margin.left - margin.right, 
-      height = 875 - margin.top - margin.bottom;  
 
+  // dimensions 
+  var margin = {top: 0, right: 70, bottom: 0, left: 10}; 
+  var width = 300 - margin.left - margin.right, 
+      height = 600 - margin.top - margin.bottom;  
+
+  // svg layer 
   var svg = d3.select('#strip').append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g'); 
 
+  // scales 
   var yScale = d3.scaleLinear()
-    .domain([95, 0])
-    .range([height, 0]); 
+    .domain([95, 90, 80, 0])
+    .range([550, 381, 164, 0]); 
 
   var xScale = d3.scaleLinear()
     .domain([0, 1])
@@ -45,6 +49,7 @@ function myStrip(caSchools) {
     .tickValues([95, 90, 80, 0])
     .tickFormat(function(d) {return d * 1 + "%"}); 
 
+  // bars  
   function getColor (d) {
     return d > 95 ? '#1696d2' : 
            d > 90 ? '#55b748' :
@@ -60,14 +65,14 @@ function myStrip(caSchools) {
     .attr('y', d => yScale(d.properties.PERCENT_jitter))
     .attr('width', 80)
     .attr('height', 0.5)
-    .attr('fill', d => getColor(d.properties.PERCENT))
+    .attr('fill', d => getColor(d.properties.PERCENT_jitter))
     .on('mouseover', d => mouseOverControl(d))
     .on('mouseout', d => mouseOutControl(d))
     .on('click', d => clickControl(d)); 
 
   svg.append('g')
     .attr('class', 'text')
-    .attr('transform', 'translate(0' + 85 + ',' + '5)')
+    .attr('transform', 'translate(0' + 85 + ',' + '4)')
     .call(yAxis);
 }; 
 
@@ -75,7 +80,7 @@ function mouseOverControl() {};
 function mouseOutControl() {}; 
 function clickControl() {}; 
 
-// MAP 
+// ---------- MAP ---------- //
 function myMap (caBase, caSchools) {
 
   // base map 
@@ -87,12 +92,18 @@ function myMap (caBase, caSchools) {
     {style: {"color": "#696969", "weight": 1}})); 
 
   // search bar 
+  // https://github.com/stefanocudini/leaflet-search/blob/master/examples/outside.html
   var markersLayer = new L.LayerGroup();
   map.addLayer(markersLayer);
+  map.addControl(new L.Control.Search(
+    {container: 'findbox', 
+    layer: markersLayer, 
+    collapsed: false, 
+    textPlaceholder: 'Search for an elementary school here...'})); 
 
   // home button 
   // https://gis.stackexchange.com/questions/127286/home-button-leaflet-map
-  L.easyButton('fa-home', function (btn, map){map.setView([37, -119], 6)}, 'Zoom To Home').addTo(map);
+  L.easyButton('fa-home', function (btn, map){map.setView([37.5, -119], 6)}, 'Zoom To Home').addTo(map);
 
   // svg layer 
   var svg = d3.select("#map").select("svg");
@@ -105,7 +116,7 @@ function myMap (caBase, caSchools) {
     d.city = d.properties.CITY
     d.enrollment = d.properties.ENROLLMENT
     d.coverage = d.properties.PERCENT
-    d.id = d.SCHOOL_CODE
+    d.id = d.properties.SCHOOL_CODE
   });
 
   // info tooltip
@@ -119,15 +130,34 @@ function myMap (caBase, caSchools) {
   };
 
   info.update = function (d) {
-      this._div.innerHTML = '<h4>KINDERGARTEN MMR COVERAGE</h4>' + (d ?
-        d.name + "<br/>" +
-        d.city + ", CA" + "<br/>" + "<br/>" + 
-        "ENROLLMENT: " + d.enrollment + "<br/>" + 
-        "COVERAGE: " + d.coverage + "%" + "<br/>" 
-        : "Hover over a school for details.");
+    this._div.innerHTML = '<h4>KINDERGARTEN MMR COVERAGE</h4>' + (d ?
+      d.name + "<br/>" +
+      d.city + ", CA" + "<br/>" + "<br/>" + 
+      "ENROLLMENT: " + d.enrollment + "<br/>" + 
+      "COVERAGE: " + d.coverage + "%" + "<br/>" 
+      : "Hover over a school for details.");
   };
 
   info.addTo(map);
+
+  // legend 
+  // https://gis.stackexchange.com/questions/133630/adding-leaflet-legend
+  var legend = L.control({position: 'bottomleft'});
+  legend.onAdd = function (map) {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+  labels = ['50 kindergarteners', '100 kindergarteners', '500 kindergarteners'];
+
+  div.innerHTML = 
+  ['<div class="row" <span class="legend inner-row"><h4>SCHOOL<br/>ENROLLMENT</span></h4></div>' +
+  '<div class="row" "><i class="circle1" ></i><span class="legend inner-row">' + labels[0] + '</span></div>' +
+  '<div class="row" "><i class="circle2" ></i><span class="legend inner-row">' + labels[1] + '</span></div>' +
+  '<div class="row" "><i class="circle3" ></i><span class="legend inner-row">' + labels[2] + '</span></div>']
+
+   return div;
+  };
+  
+  legend.addTo(map);
 
   // mouseover 
   var schoolMouseover = function (d) {
@@ -140,16 +170,16 @@ function myMap (caBase, caSchools) {
 
   var onColor = function (d) {
     svg.selectAll("circle").filter("." + this.getAttribute(d.id))
-      .style("opacity", 0.8);
+      .style('stroke', 'black')
+      .style('stroke-width', 2); 
   }; 
 
   var offColor = function (d) {
-    svg.selectAll("circle").filter("." + this.getAttribute(d.id))
-      .style("opacity", 0.8);
+    svg.selectAll("circle").filter("." + this.getAttribute(d.id)); 
   }; 
 
   var clicked = function (d) {
-    map.setView(L.latLng(d.LatLng), 10, {animate: false});
+    map.setView(L.latLng(d.LatLng), 15);
   }; 
 
   // schools   
@@ -167,7 +197,9 @@ function myMap (caBase, caSchools) {
     .attr("name", d => d.name)
     .attr("city", d => d.city)
     .attr("fill", d => getColor(d.coverage))
-    .style("opacity", 0.8) 
+    .style('stroke-width', 0.1)
+    .style('stroke', 'black')
+    .style("opacity", 0.9) 
     .attr("r", d => Math.sqrt(parseInt(d.enrollment) * 0.1))
     .attr("active", false)
     .on("click", clicked); 
@@ -204,6 +236,7 @@ function myMap (caBase, caSchools) {
   }; 
 }
 
+// ---------- OTHER SOURCES ---------- // 
 // MAP 
 // http://bl.ocks.org/d3noob/9267535
 // https://stackoverflow.com/questions/40047326/overlaying-circles-on-leaflet-with-d3-results-in-not-positioned-properly
@@ -217,4 +250,7 @@ function myMap (caBase, caSchools) {
 // https://bl.ocks.org/phocks/878340b26d0aa69658854c17cdf2e046
 // http://www.maartenlambrechts.com/2015/11/30/interactive-strip-plots-for-visualizing-demographics.html
 // https://murray-cecile.github.io/map-unemployment/ 
+
+
+
 
